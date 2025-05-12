828dd3bed7c51f55e14f19890f19c745802d02cd
David Mayes <dmayes77@gmail.com>
2025-05-12 02:23:00 -0400

    removed padding from questionnaire

--- app/questionnaire/page.jsx
+++ app/questionnaire/page.jsx
@@ -4,7 +4,7 @@ import QuestionnaireEmbed from "@/components/sections/QuestionnaireEmbed";
 
 export default function QuestionnaireWizard() {
   return (
-    <div className="max-w-3xl mx-auto p-6">
+    <div className="max-w-7xl mx-auto">
       <QuestionnaireEmbed
         surveyId="k3sG10K4LC98L5JzzEsj"
         title="Design Questionnaire"
