da8de4922596f80630f374024ea59aede1102fe4
David Mayes <dmayes77@gmail.com>
2025-05-21 15:17:56 -0400

    final update to questionnaire page

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum‑scale=1.0, user‑scalable=no"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
         <link rel="icon" href={logo.logoOnly.url} />
         <link rel="apple-touch-icon" sizes="180x180" href={logo.logoOnly.url} />
